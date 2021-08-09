74aade46d527544f8f65a7ed65b04d1d8d26b243
Sebastian Snoer <seb.snoer@gmail.com>
2021-08-09 09:53:13 +0200

    Fixed start message

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -5,7 +5,8 @@ void kernel_main() {
     print_str("Welcome to Zed OS!");
     
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("\nAn os made by");
+    
+    print_str("\n\nAn os made by");
     print_str("\n  Sebsa");
     print_str("\nAnd opensource on GitHub");
     print_str("\n  bit.ly/zedos");
