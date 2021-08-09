64bab4d63c777c816fbb56cacdfb05eb6e0f3d38
Sebastian Snoer <seb.snoer@gmail.com>
2021-08-09 11:46:17 +0200

    Added docker startup message

---
+++ buildenv/start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+printf "Welcome to Zed OS buildenv!\n\nAn os made by\n  Sebsa\nAnd opensource on GitHub\n  bit.ly/zedos\n\nTo build the os run \"make build-x86_64\"\n"
+/bin/bash
